0823513bafef4fbdff397894be6fd540abc06dca
Jonne Saleva <jonnesaleva@brandeis.edu>
2022-03-06 18:26:38 -0500

    Add pipeline.sh to combine ingest and dump

---
+++ recipes/pipeline.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+## Command line arguments
+input_json=${1}
+output_folder=${2}
+n_workers=${3:-15}
+
+## Constants
+db_name=paranames_db
+collection_name=paranames
+chunk_size=100000
+
+langs="all"
+entity_types="PER,LOC,ORG"
+default_format="tsv"
+should_collapse_languages="no"
+
+## Ingest input JSON
+rich "[bold underline]Ingesting input JSON:[/]" -p
+recipes/ingest.sh \
+    $input_json \
+    $db_name \
+    $collection_name \
+    $chunk_size \
+    $n_workers
+
+## Dump all entities in all languages
+rich "[bold underline]Dumping all PER/LOC/ORG:[/]" -p
+recipes/dump.sh \
+    $langs $input_json $entity_types \
+    $db_name $collection_type \
+    $should_collapse_languages \
+    $n_workers
